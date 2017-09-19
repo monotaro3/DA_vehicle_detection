b579e1b7267fe53a920e3d046915a2691cdcb679
Yohei Koga <monotaro_EX@hotmail.com>
2017-09-19 18:03:37 +0900

    implement a function of making training graph

---
+++ graph.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+import json
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+
+def gengraph(logfile,savedir="graph",figname = "train_graph.png"):
+    with open(logfile) as f:
+        # read json file
+        ch_log = json.load(f)
+    loss = [ep['main/loss'] for ep in ch_log]
+    loss_loc = [ep['main/loss/loc'] for ep in ch_log]
+    loss_conf = [ep['main/loss/conf'] for ep in ch_log]
+    itr = [ep['iteration'] for ep in ch_log]
+
+    if not os.path.isdir(savedir): os.makedirs(savedir)
+    savepath = os.path.join(savedir,figname)
+
+    plt.figure()
+    plt.title("Training loss")
+    plt.ylabel("Loss")
+    plt.xlabel("Iteration")
+    plt.ylim([0,0.1])
+    plt.plot(itr, loss, label="Loss")
+    plt.plot(itr, loss_loc, label="Loss_loc")
+    plt.plot(itr, loss_conf, label="Loss_conf")
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.subplots_adjust(right=0.7)
+    plt.savefig(savepath)
+    # plt.show()
+
+if __name__ == "__main__":
+    gengraph("log_300_0.16",figname="train_log_300_0.16.png")
+
