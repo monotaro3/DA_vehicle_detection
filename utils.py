3cd004fcb4af52447f736f31268389bd49affb35
Yohei Koga <monotaro_EX@hotmail.com>
2017-09-27 17:57:08 +0900

    add convertDepth function

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
 import numpy as np
+from osgeo import gdal, osr
+from chainer import serializers
 
 def gen_dms_time_str(time_sec):
     sec = time_sec % 60
@@ -28,3 +30,55 @@ def make_bboxeslist_chainercv(gt_file):
         line = gt_txt.readline()
     bboxes = np.stack(bboxes).astype(np.float32)
     return bboxes
+
+def convertDepth(input,output):
+    gdal.AllRegister()
+    src = gdal.Open(input)
+    SpaRef = src.GetProjection()
+    geoTransform = src.GetGeoTransform()
+
+    cols = src.RasterXSize
+    rows = src.RasterYSize
+    driver = gdal.GetDriverByName("GTiff")
+    dst = driver.Create(output, cols, rows, 3, gdal.GDT_Byte)
+    dst.SetGeoTransform(geoTransform)
+    dstSRS = osr.SpatialReference()
+    dstSRS.ImportFromWkt(SpaRef)
+    dst.SetProjection(dstSRS.ExportToWkt())
+    for i in range(1,4):
+        src_data_ = src.GetRasterBand(i).ReadAsArray()
+        src_data = src_data_.copy()
+        src_data[src_data >= 1024] = 1023
+        src_data = (src_data/4).astype(np.uint8)
+        dst.GetRasterBand(i).WriteArray(src_data)
+    dst = None
+
+def decode_mask2bbox(maskimg,windowsize):
+    rp = np.where(maskimg == 255)
+    h,w,c = maskimg.shape
+    dots_ = []
+    dot_num = rp[0].size
+    for i in range(dot_num):
+        dots_.append([rp[0][i]+1, rp[1][i]+1])
+    bbox = []
+    window_half = int(windowsize / 2)
+    for d in dots_:
+        xmin = d[1] - window_half
+        ymin = d[0] - window_half
+        xmax = d[1] + window_half
+        ymax = d[0] + window_half
+        if windowsize % 2 == 0:
+            xmin +=1
+            ymin +=1
+        if xmin < 1: xmin = 1
+        if ymin < 1: ymin = 1
+        if xmax > w: xmax = w
+        if ymax > h: ymax = h
+        bbox.append([xmin,ymin,xmax,ymax])
+    return bbox
+
+if __name__ == "__main__":
+    convertDepth('c:/work/spacenet\\raw\\RGB-PanSharpen_AOI_5_Khartoum_img1.tif',"c:/work/test4.tif")
+    # trainer = None
+    # serializers.load_npz("model/snapshot_iter_30000", trainer)
+    # pass
