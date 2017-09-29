8d0b0013e6995d1ce6eb8bd90b5e50884978572e
Yohei Koga <monotaro_EX@hotmail.com>
2017-09-29 17:39:53 +0900

    add discriminator(not completed)

--- SSD_for_vehicle_detection.py
+++ SSD_for_vehicle_detection.py
@@ -3,6 +3,8 @@ from chainercv.links import SSD300
 from chainercv.links import SSD512
 from chainercv.links.model.ssd.ssd_vgg16 import _check_pretrained_model, _load_npz, VGG16Extractor512, VGG16Extractor300,_imagenet_mean
 from chainercv.links.model.ssd import Multibox
+import chainer
+from chainer import Chain
 
 class SSD512_vd(SSD512):
     def __init__(self, n_fg_class=None, pretrained_model=None,defaultbox_size=(35.84, 76.8, 153.6, 230.4, 307.2, 384.0, 460.8, 537.6),mean = _imagenet_mean):
@@ -38,3 +40,9 @@ class SSD300_vd(SSD300):
 
         if path:
             _load_npz(path, self)
+
+class Discriminator(Chain):
+    def __init__(self, wscale=0.02):
+        w = chainer.initializers.Normal(wscale)
+        super(Discriminator, self).__init__()
+        with self.init_scope():
