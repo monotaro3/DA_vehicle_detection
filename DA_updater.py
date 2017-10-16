297d4194869e84fe3af9d8250790c230cdabf291
Yohei Koga <monotaro_EX@hotmail.com>
2017-10-16 18:57:46 +0900

    add DA1 updater

--- DA_updater.py
+++ DA_updater.py
@@ -3,6 +3,8 @@ import numpy as np
 import chainer
 import chainer.functions as F
 from chainer import Variable
+from chainer.dataset import convert
+from chainercv.links.model.ssd import multibox_loss
 
 
 class Updater1(chainer.training.StandardUpdater):
@@ -122,3 +124,62 @@ class Updater2(chainer.training.StandardUpdater):
 
         chainer.reporter.report({'loss_t_enc': loss_t_enc})
         chainer.reporter.report({'loss_dis': loss_dis})
+
+class DA_updater1(chainer.training.StandardUpdater):
+    def __init__(self, *args, **kwargs):
+        self.dis, self.cls = kwargs.pop('models')
+        super(DA_updater1, self).__init__(*args, **kwargs)
+        self.t_enc = self.cls.extractor
+        self.alpha = 1
+        self.k = 3
+
+    def update_core(self):
+        #t_enc_optimizer = self.get_optimizer('opt_t_enc')
+        dis_optimizer = self.get_optimizer('opt_dis')
+        cls_optimizer = self.get_optimizer('opt_cls')
+        xp = self.dis.xp
+
+        batch_source = self.get_iterator('main').next()
+        batch_source_array = convert.concat_examples(batch_source,self.device)
+        batch_target = self.get_iterator('target').next()
+        batchsize = len(batch_source)
+
+        #compute forwarding with source data
+        src_fmap = self.t_enc(batch_source_array[0]) #src feature map
+        mb_locs, mb_confs = self.cls.multibox(src_fmap)
+        loc_loss, conf_loss = multibox_loss(
+            mb_locs, mb_confs, batch_source_array[1], batch_source_array[2], self.k)
+        cls_loss = loc_loss * self.alpha + conf_loss #cls loss
+
+        y_source = self.dis(src_fmap)
+
+        tgt_fmap = self.t_enc(Variable(xp.array(batch_target)))
+        y_target = self.dis(tgt_fmap)
+
+        # z = Variable(xp.asarray(self.gen.make_hidden(batchsize)))
+        # x_fake = self.gen(z)
+        # y_fake = self.dis(x_fake)
+
+        # loss_dis = F.sum(F.softplus(-y_real)) / batchsize
+        # loss_dis += F.sum(F.softplus(y_fake)) / batchsize
+        loss_dis = F.sum(F.softplus(-y_source)) / batchsize
+        loss_dis += F.sum(F.softplus(y_target)) / batchsize
+
+        loss_t_enc = F.sum(F.softplus(-y_target)) / batchsize
+
+        #update cls(and t_enc) by cls_loss and loss_t_enc
+        self.cls.cleargrads()
+        cls_loss.backward()
+        loss_t_enc.backward()
+        cls_optimizer.update()
+        for s_map, t_map  in zip(src_fmap, tgt_fmap):
+             s_map.unchain_backward()
+             t_map.unchain_backward()
+
+        self.dis.cleargrads()
+        loss_dis.backward()
+        dis_optimizer.update()
+
+        chainer.reporter.report({'loss_t_enc': loss_t_enc})
+        chainer.reporter.report({'loss_dis': loss_dis})
+        chainer.reporter.report({'loss_cls': cls_loss})
